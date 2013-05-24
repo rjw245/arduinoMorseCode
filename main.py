6c652941e02dd4d8f828311de9560871eadaabbd
rjw245 <rjw245@gmail.com>
2013-05-24 11:15:29 -0300

    Cleaned up commented out lines

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ arduino = serialcom.Arduino()
 while not arduino.isReady():
 	pass #Spin while arduino not ready
 
-# arduino.turnOn()
-# time.sleep(1)
-# arduino.turnOff()
-
-# time.sleep(2)
-
 string = sys.argv[1]
 morseGen = morse.MorseCode(arduino)
 morseGen.writeString(string)
